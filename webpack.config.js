56ee01e82e12ca65f8cf8bf796d25ca16f949e4d
Emmanuel Daniel <ecomje@gmail.com>
2019-09-29 17:06:41 +0100

    enable file loader

--- webpack.config.js
+++ webpack.config.js
@@ -47,6 +47,17 @@ module.exports = {
         ]
       },
       {
+        test: /\.(png|jp(e*)g|svg|gif|ttf|woff|woff2|eot)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "images/[hash]-[name].[ext]"
+            }
+          }
+        ]
+      },
+      {
         test: /\.html$/,
         use: [
           {
